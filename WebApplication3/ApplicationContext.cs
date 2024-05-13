cde7ba2b91e40e28f4003035028f575965548d53
Алексей Боронин <boronin_1993@mail.ru>
2024-05-13 20:45:50 +0300

    Добавьте файлы проекта.

---
+++ WebApplication3/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication3
+{
+    public class ApplicationContext:DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public ApplicationContext(DbContextOptions<ApplicationContext> options):base(options) 
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User { Id=1,Name="Ted",Age=35},
+                new User { Id=2, Name="Bred",Age=40},
+                new User { Id=3, Name="Tom",Age=30}
+                );
+        }
+    }
+}
---
+++ WebApplication3/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication3;
+
+var builder = WebApplication.CreateBuilder();
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");//"Server=
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapGet("/api/users", async (ApplicationContext db)=> await db.Users.ToListAsync());
+
+app.MapGet("/api/users/{id:int}", async(int id, ApplicationContext db) =>
+{
+    User? user= await db.Users.FirstOrDefaultAsync(x=>x.Id== id);
+
+    if (user == null) return Results.NotFound(new { message = "Пользователь не найден" });
+    return Results.Json(user);
+});
+
+app.MapDelete("/api/users/{id:int}", async (int id, ApplicationContext db) =>
+{
+    User? user = await db.Users.FirstOrDefaultAsync(x=>x.Id == id);
+    if (user == null) return Results.NotFound(new { message = "Пользователь не найден" });
+
+    db.Users.Remove(user);
+    await db.SaveChangesAsync();
+    return Results.Json(user);
+});
+
+app.MapPost("/api/users", async (User user, ApplicationContext db) =>
+{
+    await db.Users.AddAsync(user);
+    await db.SaveChangesAsync();
+    return user;
+});
+
+app.MapPut("/api/users", async (User userData, ApplicationContext db) =>
+{
+    var user=await db.Users.FirstOrDefaultAsync(x=>x.Id==userData.Id);
+
+    if (user == null) return Results.NotFound(new { message = "Пользователь не найден" });
+
+    user.Age=userData.Age;
+    user.Name=userData.Name;
+    await db.SaveChangesAsync();
+    return Results.Json(user);
+});
+
+app.Run();
